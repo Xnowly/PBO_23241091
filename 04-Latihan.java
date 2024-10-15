5e4d1a881cffe1fe1e7e9ef30dcdf4f5c7127940
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-10-16 06:23:42 +0800

    Update 04-Latihan.java

--- 04-Latihan.java
+++ 04-Latihan.java
@@ -1,38 +1,35 @@
-class Mahasiswa {
-    //Membuat atribut untuk objek
-    public String nama;
-    public String nim;
-    public String prodi;
-    public int umur;
-    public double ipk;
-
-    //Constructor
-    //namanya sama dengan class
-    //tidak punya return value
-    //method yang pertama kali dipanggil saat objek dipanggil
-    public Mahasiswa(String Inputnama, String Inputnim, String Inputprodi, int Inputumur, double Inputipk) {
-        //isi data atribut
-        nama = Inputnama;
-        nim = Inputnim;
-        prodi = Inputprodi;
-        umur = Inputumur;
-        ipk = Inputipk;
-
-        //cetak data
-        System.out.println("Nama: " + nama);
-        System.out.println("NIM: " + nim);
-        System.out.println("Prodi: " + prodi);
-        System.out.println("umur: " + umur);
-        System.out.println("ipk: " + ipk);
-
+class Kalkulator{
+    //membuat konstruksi
+    Kalkulator(){
+  
+    }
+  
+    //membuat method
+    int add(int a, int b){
+        
+      return a + b;
+    }
+    
+    int subtract(int a, int b){
+      return a - b;
+    }
+  
+    int multiply(int a, int b){
+      return a * b;
+    }
+  
+    int device(int a, int b){
+      return a / b;
     }
 }
-
-public class App2 {
-        public static void main(String[] args) throws Exception {
-
-        Mahasiswa mhs_1 = new Mahasiswa(
-            "Putra", "23241091", "PTI", 19, 4.0);
-
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Kalkulator kalk = new Kalkulator();
+        System.out.println("Penjumlahan : " + kalk.add(5, 7));
+        System.out.println("Pengurangan : " + kalk.subtract(45, 11));
+        System.out.println("Perkalian : " + kalk.multiply(1, 3));
+        System.out.println("Pembagian : " + kalk.device(4, 2));
     }
+
 }
