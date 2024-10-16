31ec3c1a3390fd2102737a5b750fa53c6cfc802d
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-10-16 10:17:29 +0800

    Update 04-TPM.java

--- 04-TPM.java
+++ 04-TPM.java
@@ -1,35 +1,51 @@
-class Kalkulator{
-    //membuat konstruksi
-    Kalkulator(){
-  
-    }
-  
-    //membuat method
-    int add(int a, int b){
-        
-      return a + b;
-    }
-    
-    int subtract(int a, int b){
-      return a - b;
-    }
-  
-    int multiply(int a, int b){
-      return a * b;
-    }
-  
-    int device(int a, int b){
-      return a / b;
-    }
-}
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Kalkulator kalk = new Kalkulator();
-        System.out.println("Penjumlahan : " + kalk.add(5, 7));
-        System.out.println("Pengurangan : " + kalk.subtract(45, 11));
-        System.out.println("Perkalian : " + kalk.multiply(1, 3));
-        System.out.println("Pembagian : " + kalk.device(4, 2));
+class Kalkulator {
+    // Deklarasi atribut
+    int bilanganPertama;
+    int bilanganKedua;
+
+    // Konstruktor dengan parameter untuk inisialisasi
+    Kalkulator(int inputBilanganPertama, int inputBilanganKedua) {
+        bilanganPertama = inputBilanganPertama;
+        bilanganKedua = inputBilanganKedua;
     }
 
+    // Method untuk penjumlahan
+    int add() {
+        return bilanganPertama + bilanganKedua;
+    }
+
+    // Method untuk pengurangan
+    int subtract() {
+        return bilanganPertama - bilanganKedua;
+    }
+
+    // Method untuk perkalian
+    int multiply() {
+        return bilanganPertama * bilanganKedua;
+    }
+
+    // Method untuk pembagian
+    int divide() {
+        if (bilanganKedua != 0) {
+            return bilanganPertama / bilanganKedua;
+        } else {
+            System.out.println("Error: Tidak bisa membagi dengan nol.");
+            return 0; // Nilai default jika pembagian dengan nol
+        }
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Membuat objek Kalkulator dengan nilai 20 dan 5
+        Kalkulator kalkulator = new Kalkulator(20, 5);
+
+        // Mencetak hasil dari berbagai operasi aritmatika
+        System.out.println("Bilangan Pertama : " + kalkulator.bilanganPertama);
+        System.out.println("Bilangan Kedua : " + kalkulator.bilanganKedua);
+        System.out.println("Penjumlahan : " + kalkulator.add());
+        System.out.println("Pengurangan : " + kalkulator.subtract());
+        System.out.println("Perkalian : " + kalkulator.multiply());
+        System.out.println("Pembagian : " + kalkulator.divide());
+    }
 }
