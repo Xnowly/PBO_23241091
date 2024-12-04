9ad3d426ceda0110b66c9fc4f3b93058378de678
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-12-04 10:00:35 +0800

    Add files via upload

---
+++ Modul 4/09-StaticMethodDanVariabel/java/src/App.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+
+class Player {
+    //variabel untuk objek
+    private String nama;
+    //static variabel
+    private Static int numberOfPlayer;
+    //static Variabel untuk menampung nama
+    private static ArrayList <String> namaList = new ArrayList<String>();
+
+
+    //konstruktor
+    Player(String nama){
+        this.nama = nama;
+        Player.numberOfPlayer++;
+        Player.namaList.add(this.nama);
+        
+    }
+//method untuk membaca  private
+void cetak()
+}
+
+}
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Player3 = new Player (nama:"Miyuki")
+        Player4 =
+        //membuat objek baru
+        Player player1 = new Player ("nama: koyaku ")
+        Player player2 = new Player ("Nanatsu")
+        Player player3 = new Player ("Garo")
+        Player player4 = new Player ("Hatsune Miku")
+
+        //menerapkan static Variabel dan Method
+        player.showNumberPlayer();
+
+
+    
+       
+    }
+}
