6b3bbe8269199b0865a56e27453f4b5ee80e5928
Xnowly <147676111+Xnowly@users.noreply.github.com>
2024-12-04 18:59:25 +0800

    App.java

--- Modul 4/09-StaticMethodDanVariabel/java/src/App.java
+++ Modul 4/09-StaticMethodDanVariabel/java/src/App.java
@@ -1,47 +1,53 @@
 import java.util.ArrayList;
 
-
 class Player {
     //variabel untuk objek
     private String nama;
-    //static variabel
-    private Static int numberOfPlayer;
-    //static Variabel untuk menampung nama
-    private static ArrayList <String> namaList = new ArrayList<String>();
 
+    //Static Variabel
+    private static int numberPlayer;
 
-    //konstruktor
-    Player(String nama){
+    //static variabel menampung nama
+    private static ArrayList<String> namaList = new ArrayList<String>();
+
+    
+    //constructor
+    Player (String nama){
         this.nama = nama;
-        Player.numberOfPlayer++;
+        Player.numberPlayer++; //add object menambah objek
         Player.namaList.add(this.nama);
-        
     }
-//method untuk membaca  private
-void cetak()
+
+    // method untuk membaca variabel private
+    void cetak(){
+        System.out.println("Nama : " + this.nama);
+    }
+
+    //static method
+    static void shownumberPlayer(){
+        System.out.println("Jumlah Player : " + Player.numberPlayer);
+    }
+    
+    static ArrayList<String> getNama(){
+       return Player.namaList;
+    }
 }
-
-}
-
-
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { //static method punya class
+        System.out.println("Hello, World!");
+        // Membuat object baru
+        Player player1 = new Player("Hatsune Miyu");
+        Player player2 = new Player("Kyoto");
+        Player player3 = new Player("Light Yagami");
+        Player player4 = new Player("Asta");
+        Player player5 = new Player("Yuno");
+        
+        // menerapkan static variabel dan method
+        Player.shownumberPlayer();
 
-        Player3 = new Player (nama:"Miyuki")
-        Player4 =
-        //membuat objek baru
-        Player player1 = new Player ("nama: koyaku ")
-        Player player2 = new Player ("Nanatsu")
-        Player player3 = new Player ("Garo")
-        Player player4 = new Player ("Hatsune Miku")
-
-        //menerapkan static Variabel dan Method
-        player.showNumberPlayer();
-
-
-    
-       
+        // menampilkan semua nama player
+        System.out.println(Player.getNama());
     }
 }
